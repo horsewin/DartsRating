ae616af482a7935f3e9aa1f0ff09697ca9dc181a
AtsushiUMAKATSU <mamamatz@gmail.com>
2018-03-04 14:24:15 +0900

    feat at once

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,55 +1,108 @@
 'use strict';
 
+//------------------------------------------------------
+// ライブラリ定義
+//------------------------------------------------------
 /**
- * アレクサのSDKライブラリ
+ * Alexa開発に用いるSDKライブラリ
  */
-const Alexa = require("alexa-sdk");
+let Alexa = require('alexa-sdk');
 
+/**
+ * 応答を組み立てるためのライブラリ
+ */
+let util = require('util');
+
+//------------------------------------------------------
+// 変数・定数定義
+//------------------------------------------------------
 /**
  * メッセージ格納変数
  */
 const MESSAGE = require("./message");
 
 /**
- *
+ * 状態定義クラス
+ */
+const state = require("../dataAssets/state.json");
+
+/**
+ * Alexaの処理定義
+ * 各状態をハンドリングするハンドラーを追加する必要がある
  * @param event
  * @param context
  */
 exports.handler = function(event, context) {
     let alexa = Alexa.handler(event, context);
     alexa.appId = process.env.APP_ID;
-    alexa.registerHandlers(handlers);
+    alexa.registerHandlers(
+        NewSessionHandler,
+        TypeHandler
+    );
     alexa.execute();
 };
 
 //
-let handlers = {
+let NewSessionHandler = {
     'LaunchRequest': function () {
-        this.emit('SayHello');
+        this.emit('TypeIntent');
     },
-    'SayHello': function () {
+    'TypeIntent': function () {
         this.response.speak('Hello World!')
-                     .cardRenderer('hello world', 'hello world');
+            .cardRenderer('hello world', 'hello world');
         this.emit(':responseReady');
+    },
+    'TypeOnlyIntent': function () {
+        this.emit('TypeIntent');
     },
     'SessionEndedRequest' : function() {
         console.log('Session ended with reason: ' + this.event.request.reason);
     },
+    'AMAZON.HelpIntent': function () {
+        this.response.speak(MESSAGE.help.speechOutput)
+            .listen(MESSAGE.help.repromptText);
+        this.emit(':responseReady');
+    },
     'AMAZON.StopIntent' : function() {
         this.emit("AMAZON.CancelIntent");
+    },
+    'AMAZON.CancelIntent': function () {
+        this.response.speak(MESSAGE.session.end.speechOutput);
+        this.emit(':responseReady');
+    },
+    'Unhandled': function () {
+        this.response.speak(MESSAGE.unhandled.speechOutput)
+            .listen(MESSAGE.unhandled.repromptText);
+        this.emit(":responseReady");
+    }
+};
+
+let TypeHandler = Alexa.CreateStateHandler(state.TYPE_SELECT, {
+    'TypeOnlyIntent': function () {
+        this.emit("TypeIntent");
+    },
+    'TypeIntent': function () {
+        this.response.speak('Hello World!')
+            .cardRenderer('hello world', 'hello world');
+        this.emit(':responseReady');
+    },
+    'SessionEndedRequest': function () {
+        console.log('Session ended with reason: ' + this.event.request.reason);
     },
     'AMAZON.HelpIntent' : function() {
         this.response.speak(MESSAGE.help.speechOutput)
             .listen(MESSAGE.help.repromptText);
         this.emit(':responseReady');
     },
+    'AMAZON.StopIntent': function () {
+        this.emit("AMAZON.CancelIntent");
+    },
     'AMAZON.CancelIntent' : function() {
-        this.response.speak(MESSAGE.session.end.speechOutput);
-        this.emit(':responseReady');
+        this.emit("AMAZON.CancelIntent");
     },
     'Unhandled' : function() {
         this.response.speak(MESSAGE.unhandled.speechOutput)
             .listen(MESSAGE.unhandled.repromptText);
         this.emit(":responseReady");
     }
-};
+});
